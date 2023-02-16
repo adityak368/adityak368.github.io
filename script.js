a9e833a4d6d27ef08b37e93a4a4d62746e4eee33
adityak368 <adityak368@gmail.com>
2023-02-16 09:40:28 +0100

    Updated Resume

--- script.js
+++ script.js
@@ -100,9 +100,7 @@ class App extends React.Component {
 
 
     () => {
-      const url = 'https://api.nytimes.com/svc/topstories/v2/world.json?api-key=1DXptGlOlyf3XOLM5EI0DRM9h19KLWLS';
-      const req = new Request(url);
-      return fetch(req).
+      return fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=j6USnlP2AAWJOHhCvH6e0F30PJBcltG6').
       then(response => response.json());
     });this.resumeRef = React.createRef();}getRandomArticle() {const { articles } = this.state;if (!articles) {return {};}const min = 0;const max = articles.length - 1;const index = Math.floor(Math.random() * (max - min)) + min;return articles[index];}componentDidMount() {setTimeout(async () => {const news = await this.fetchNews();if (news.status === "OK" && Array.isArray(news.results)) {this.setState({ isLoading: false, articles: news.results });} else {this.setState({ isLoading: false, isNewsFetchError: true });}}, 1000);}
 
@@ -142,7 +140,7 @@ class App extends React.Component {
       React.createElement("div", { style: { padding: '5px' } }, /*#__PURE__*/
       React.createElement("h5", { style: { margin: '5px 0px' } }, "FOR HIRE!"), /*#__PURE__*/
       React.createElement("p", null, /*#__PURE__*/React.createElement("strong", null, "Lead Software Engineer / Engineering Manager")), /*#__PURE__*/
-      React.createElement("p", null, "A Passionate Software Engineer with 8+ years of skin in the game is available for hire in just 3 months notice. He has worked in some of the top companies in the world and delivered products for Small Businesses to Big Enterprise Customers. Reach him today at ", /*#__PURE__*/React.createElement("a", { href: "mailto:adityak368@gmail.com" }, "adityak368@gmail.com"), " or via ", /*#__PURE__*/React.createElement("a", { href: "https://linkedin.com/in/aditya-k-b896165a" }, /*#__PURE__*/React.createElement("img", { width: "25px", height: "25px", src: "https://media-exp1.licdn.com/dms/image/C4E0BAQGBBp0LaObq0A/company-logo_200_200/0?e=2159024400&v=beta&t=tyrD06lDsdOelMjanHDd2gC1iqekh_JWTaXny9bAT4o" })), ". Get to know him ", /*#__PURE__*/React.createElement("a", { href: '#resumeDetails' }, "here."))))), /*#__PURE__*/
+      React.createElement("p", null, "A Passionate Full-Stack Engineer and a Engineering Leader with 8+ years of skin in the game is available for hire in just 3 months notice. He has worked in some of the top companies in the world and delivered products for Small Businesses to Big Enterprise Customers. Reach him today at ", /*#__PURE__*/React.createElement("a", { href: "mailto:adityak368@gmail.com" }, "adityak368@gmail.com"), " or via ", /*#__PURE__*/React.createElement("a", { href: "https://linkedin.com/in/aditya-k-b896165a" }, /*#__PURE__*/React.createElement("img", { width: "25px", height: "25px", src: "https://media-exp1.licdn.com/dms/image/C4E0BAQGBBp0LaObq0A/company-logo_200_200/0?e=2159024400&v=beta&t=tyrD06lDsdOelMjanHDd2gC1iqekh_JWTaXny9bAT4o" })), ". Get to know him ", /*#__PURE__*/React.createElement("a", { href: '#resumeDetails' }, "here."), " (", /*#__PURE__*/React.createElement("a", { href: "https://drive.google.com/file/d/11VF4HT-9naWFEO6u-R7Mp_mgTIfaNOCz/view?usp=share_link" }, "Resume"), ")")))), /*#__PURE__*/
 
 
 
@@ -163,29 +161,29 @@ class App extends React.Component {
       React.createElement("h3", { className: "heading" }, "ADVERTISEMENTS"), /*#__PURE__*/
       React.createElement("div", { style: { padding: '5px' } }, /*#__PURE__*/
       React.createElement("h3", null, "Meet Aditya!"), /*#__PURE__*/
-      React.createElement("p", null, "Aditya loves to code and build complex software. He has worked in software development for 6+ years. He does not limit himself to any particular domain/component/tech stack and considers himself a ", /*#__PURE__*/React.createElement("strong", null, "generalist and language agnostic"), ". He does whatever it takes to get the job done."), /*#__PURE__*/
-      React.createElement("p", null, "He also likes to solve algorithmic problems. He likes to optimize the code he writes and also come up with good designs. He considers himself a ", /*#__PURE__*/React.createElement("strong", null, "full stack developer"), " as he has developed applications for mobile/desktop/web and has good idea about the complete ecosystem."), /*#__PURE__*/
+      React.createElement("p", null, "Aditya loves to build complex software. He has worked in software development for 8+ years. He does not limit himself to any particular domain/component/tech stack and considers himself a ", /*#__PURE__*/React.createElement("strong", null, "generalist and language agnostic"), " and also a ", /*#__PURE__*/React.createElement("strong", null, "full stack engineer"), ". He also likes to solve algorithmic problems. He likes to optimize the code he writes and also come up with good designs. He does whatever it takes to get the job done."), /*#__PURE__*/
+      React.createElement("p", null, "He is also an Engineering leader. He has experience leading projects from initiation to delivery and has developed applications for mobile/desktop/web and has good idea about the end to end ecosystem."), /*#__PURE__*/
       React.createElement("h4", null, "Thats all good, but where has he worked till now? and what has he done?"), /*#__PURE__*/
       React.createElement("ul", { className: "nobullets" }, /*#__PURE__*/
       React.createElement("li", null, /*#__PURE__*/
-
-
-
-
-
-
-
+      React.createElement(Card, {
+        title: 'Software Engineer @ Amazon Web Services (AWS)',
+        subtitle: 'May 2022 - Present',
+        imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/800px-Amazon_Web_Services_Logo.svg.png',
+        linkUrl: 'https://aws.amazon.com/',
+        description: /*#__PURE__*/
+        React.createElement("p", null, "He was responsible for leading the development of a new greenfield AWS Service. He designed the system end to end from scratch. his responsibilities include writing the design doc that details the high level design, low level design, database modelling, frontend design etc. Other responsibilities include planning effort estimates, mentoring, implementation and delivery. He was also part of the hiring team and helped in interview efforts and feedback/decision discussions") }), /*#__PURE__*/
 
 
 
 
       React.createElement(Card, {
-        title: 'Senior/Lead Software Engineer @ Smart Recruiters',
-        subtitle: 'Sep 2021 - Present',
+        title: 'Lead Software Engineer @ Smart Recruiters',
+        subtitle: 'Sep 2021 - May 2022',
         imgUrl: 'https://www.firstbird.com/wp-content/uploads/2019/09/smartrecruiters.png',
         linkUrl: 'https://www.smartrecruiters.com',
         description: /*#__PURE__*/
-        React.createElement("p", null, "In Smartrecruiters he was responsible for the development of the ", /*#__PURE__*/React.createElement("a", { href: "https://www.smartrecruiters.com/recruiting-software/smartpal/" }, "Smartpal"), " Chatbot. He lead the migration of the chatbot from JS to TS. The migration included analyzing multiple codebases, planning, setting milestones with stakeholders, creating stories and delegating them to team members. He also improved the codebase by refactoring major parts, adding tests, modularizing the code and adding documentation.") }), /*#__PURE__*/
+        React.createElement("p", null, "In Smartrecruiters he lead the frontend development of the ", /*#__PURE__*/React.createElement("a", { href: "https://www.smartrecruiters.com/recruiting-software/smartpal/" }, "Smartpal"), " Chatbot. He was responsible for the migration of the chatbot from JS to TS. The migration included analyzing multiple codebases, planning, setting milestones with stakeholders, creating stories and delegating them to team members. He also improved the codebase by refactoring major parts, adding tests, modularizing the code and adding documentation.") }), /*#__PURE__*/
 
 
 
@@ -196,7 +194,7 @@ class App extends React.Component {
         imgUrl: 'https://static.teamviewer.com/resources/2019/07/TeamViewer_Logo_512x512.png',
         linkUrl: 'https://www.teamviewer.com',
         description: /*#__PURE__*/
-        React.createElement("p", null, "He designed and developed features for the enterprise division of TeamViewer called ", /*#__PURE__*/React.createElement("a", { href: "https://www.teamviewer.com/en/teamviewer-tensor/" }, "TeamViewer Tensor"), ". Primarily he worked on the web portal built with React/Typescript and C#. He also worked on the ", /*#__PURE__*/React.createElement("a", { href: "https://www.teamviewer.com/en/" }, "TeamViewer"), " client written in C++. His other responsibilities include mentoring juniors and reviewing code. He also developed and introduced many internal tools to speedup development and testing.") }), /*#__PURE__*/
+        React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("p", null, "He designed and developed features for the enterprise division of TeamViewer called ", /*#__PURE__*/React.createElement("a", { href: "https://www.teamviewer.com/en/teamviewer-tensor/" }, "TeamViewer Tensor"), ". Primarily he worked on the web portal built with React/Typescript and C#. He also worked on the ", /*#__PURE__*/React.createElement("a", { href: "https://www.teamviewer.com/en/" }, "TeamViewer"), " client written in C++. His other responsibilities include mentoring juniors and reviewing code. He also developed and introduced many internal tools to speedup development and testing. "), /*#__PURE__*/React.createElement("p", null, "List of released features: ", /*#__PURE__*/React.createElement("a", { href: "https://www.teamviewer.com/en/teamviewer-tensor/comprehensive-activity-logging/" }, "Auditability"), ", ", /*#__PURE__*/React.createElement("a", { href: "https://www.teamviewer.com/en/teamviewer-tensor/conditional-access/" }, "Conditional Access"), ", ", /*#__PURE__*/React.createElement("a", { href: "https://www.teamviewer.com/en/custom-modules/" }, "Custom Modules"), ", ", /*#__PURE__*/React.createElement("a", { href: "https://www.teamviewer.com/en/features/automated-mass-deployment/" }, "Mass Deployment"), " and Assignment")) }), /*#__PURE__*/
 
 
       React.createElement(Card, {
@@ -216,8 +214,8 @@ class App extends React.Component {
 
 
       React.createElement("h4", null, "Cool. But the position needs a speciality and i do not know if he has any experience with our stack."), /*#__PURE__*/
-      React.createElement("p", null, "Aditya is comfortable with ", /*#__PURE__*/
-      React.createElement("strong", null, "C++/Qt, Java, React/Typescript/Nodejs and Go"), ". His C++/Qt experience comes from all the three companies he has worked in whereas React/Typescript/Nodejs comes from TeamViewer (This site is written in React). Java from Scalable and by his Android App Development in Tektronix and on a personal level (10 apps in Google Play store) and Go from personal projects."), /*#__PURE__*/
+      React.createElement("p", null, "Aditya is experienced in leading projects in SmartRecruiters and AWS. He is comfortable with ", /*#__PURE__*/
+      React.createElement("strong", null, "C++/Qt, Java, React/Typescript/Nodejs and Go"), ". His C++/Qt experience comes from Tektronix and Intel whereas React/Typescript/Nodejs comes from TeamViewer, Intel, AWS, SmartRecruiters (This site is written in React). Java from AWS and by his Android App Development in Tektronix and on a personal level (10 apps in Google Play store) and Go from personal projects."), /*#__PURE__*/
 
 
       React.createElement("p", null, "He is also familiar with the most popular IDEs, CI/CD and agile processes. He follows Scrum for his day to day activities."), /*#__PURE__*/
@@ -231,7 +229,7 @@ class App extends React.Component {
       React.createElement("li", null, "Hardworking and dedicated individual who is open to learning and can get stuff done."), /*#__PURE__*/
       React.createElement("li", null, "A partner in crime to travel, play badminton/volleyball watch MMA or go to an Eminem concert.")), /*#__PURE__*/
 
-      React.createElement("a", { href: "https://drive.google.com/file/d/1dA76X6GVjZErgk16ejL0zuMpV236PgUA/view" }, "Download Resume"), /*#__PURE__*/
+
       React.createElement("h4", null, "Have a look at his works @ ", /*#__PURE__*/React.createElement("a", { href: "https://github.com/adityak368" }, "Github"), " and ", /*#__PURE__*/React.createElement("a", { href: "https://play.google.com/store/apps/developer?id=Aditya+K&hl=en_IN" }, "Google Play")))),
 
 
